52752f8f4d5a0ce39e7d8a84668e89d749305e7a
Sara-Alcaras <saraalcaras401@gmail.com>
2020-06-23 18:42:30 -0300

    css mobile incidents

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { NavagationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navagation/stack';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const AppStack = createStackNavigator();
 
@@ -10,7 +10,7 @@ import Detail from './pages/Detail';
 export default function Routes() {
     return (
         <NavagationContainer>
-            <AppStack.Navigator>
+            <AppStack.Navigator screenOptions={{ headerShown: false }}>
                 <AppStack.Screen name="Incidents" component={Incidents} />
                 <AppStack.Screen name="Detail" component={Detail} />
 
